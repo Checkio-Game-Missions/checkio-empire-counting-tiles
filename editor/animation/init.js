f46edc7fb1c36699997a8f07e39edf69ec497d43
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-10-15 17:33:08 +1200

    animation

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -77,41 +77,104 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
             }
             //Dont change the code before it
 
-            //Your code here about test explanation animation
-            //$content.find(".explanation").html("Something text for example");
-            //
-            //
-            //
-            //
-            //
+            var canvas = new CountingTilesCanvas($content.find(".explanation")[0], checkioInput, 8);
+            canvas.createCanvas();
+
 
 
             this_e.setAnimationHeight($content.height() + 60);
 
         });
 
-       
+        var $tryit;
+        var tooltip = false;
+//
+        ext.set_console_process_ret(function (this_e, ret) {
+            $tryit.find(".checkio-result-in").html(ret);
+        });
 
-        var colorOrange4 = "#F0801A";
-        var colorOrange3 = "#FA8F00";
-        var colorOrange2 = "#FAA600";
-        var colorOrange1 = "#FABA00";
+        ext.set_generate_animation_panel(function (this_e) {
 
-        var colorBlue4 = "#294270";
-        var colorBlue3 = "#006CA9";
-        var colorBlue2 = "#65A1CF";
-        var colorBlue1 = "#8FC7ED";
+            $tryit = $(this_e.setHtmlTryIt(ext.get_template('tryit')));
+            var tCanvas = new CountingTilesCanvas($tryit.find(".tryit-canvas")[0], 2, 6);
+            tCanvas.createCanvas();
+            tCanvas.createFeedback(function(r, e) {
+                this_e.sendToConsoleCheckiO(r);
+                e.stopPropagation();
+                return false;
+            });
+            $tryit.find(".tryit-canvas").mouseenter(function (e) {
+                if (tooltip) {
+                    return false;
+                }
+                var $tooltip = $tryit.find(".tryit-canvas .tooltip");
+                $tooltip.fadeIn(1000);
+                setTimeout(function () {
+                    $tooltip.fadeOut(1000);
+                }, 2000);
+                tooltip = true;
+            });
 
-        var colorGrey4 = "#737370";
-        var colorGrey3 = "#9D9E9E";
-        var colorGrey2 = "#C5C6C6";
-        var colorGrey1 = "#EBEDED";
+        });
 
-        var colorWhite = "#FFFFFF";
-        //Your Additional functions or objects inside scope
-        //
-        //
-        //
+        function CountingTilesCanvas(dom, dataInput, cellN){
+            //Vars
+            var cellSize = 40;
+            var zx = 20;
+            var zy = 20;
+            var fullX = cellSize * cellN + zx * 2;
+            var fullY = cellSize * cellN + zy * 2;
+
+            var delay = 200;
+
+            var colorDark = "#294270";
+            var colorOrange = "#F0801A";
+            var colorBlue = "#69B3E3";
+            var colorWhite = "#FFFFFF";
+
+            var attrCircle = {"stroke": colorDark, "fill": colorOrange, "stroke-width": 2};
+            var attrMain = {"stroke": colorDark, "stroke-width": 2, "fill": colorBlue};
+            var attrLine = {"stroke": colorDark, "stroke-width": 2};
+            var attrCentralLine = {"stroke": colorDark, "stroke-width": 3};
+            var attrPanel = {"stroke": colorDark, "fill": colorWhite, "opacity": 0};
+
+            var paper;
+            var mainCircle;
+
+            this.createCanvas = function() {
+                paper = Raphael(dom, fullX, fullY, 0, 0);
+                paper.rect(zx, zy, cellN * cellSize, cellN * cellSize).attr(attrMain);
+                mainCircle = paper.circle(fullX / 2, fullY / 2, dataInput * cellSize).attr(attrCircle);
+                for (var i = 1; i < cellN; i++) {
+                    paper.path(Raphael.format("M{0},{1}L{2},{1}",
+                        zx, zy + cellSize * i, zx + cellSize * cellN)).attr(i !== cellN / 2 ? attrLine : attrCentralLine);
+                    paper.path(Raphael.format("M{0},{1}L{0},{2}",
+                        zx  + cellSize * i, zy, zy + cellSize * cellN)).attr(i !== cellN / 2 ? attrLine : attrCentralLine);
+                }
+            };
+
+            this.createFeedback = function(callback) {
+                if (!mainCircle) {
+                    return false;
+                }
+                var activeElement = paper.rect(zx, zy, cellN * cellSize, cellN * cellSize).attr(attrPanel);
+                activeElement.toFront();
+                activeElement.click(function(e) {
+                    var x = ((e.offsetX || e.layerX) - zx) / cellSize;
+                    var y = ((e.offsetY || e.layerY) - zy) / cellSize;
+                    var cx = cellN / 2;
+                    var radius = Math.round(Math.sqrt(Math.pow(x - cx, 2) + Math.pow(y - cx, 2)) * 100) / 100;
+                    if (radius > cellN / 2) {
+                        radius = cellN / 2;
+                    }
+                    mainCircle.animate({"r": radius * cellSize}, delay);
+                    if (callback) {
+                        callback(radius, e);
+                    }
+                });
+
+            }
+        }
 
 
     }
