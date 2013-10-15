18823fdf8288e76f83630c93863f82a3863936c4
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-10-15 14:57:45 +1200

    referee and tests

--- verification/tests.py
+++ verification/tests.py
@@ -7,30 +7,74 @@ Each test is dict with
     "explanation" -- not necessary key, it's using for additional info in animation.
 """
 
-
 TESTS = {
     "Basics": [
         {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
+            "input": 2,
+            "answer": [4, 12]
         },
         {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
+            "input": 3,
+            "answer": [16, 20]
+        },
+        {
+            "input": 2.1,
+            "answer": [4, 20]
+        },
+        {
+            "input": 2.5,
+            "answer": [12, 20]
+        },
+        {
+            "input": 1,
+            "answer": [0, 4]
+        },
+        {
+            "input": 4,
+            "answer": [32, 28]
+        },
+        {
+            "input": 0.5,
+            "answer": [0, 4]
+        },
+        {
+            "input": 3.5,
+            "answer": [24, 28]
+        },
+        {
+            "input": 3.8,
+            "answer": [32, 28]
         }
     ],
     "Extra": [
+
         {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
+            "input": 1.1,
+            "answer": [0, 12]
         },
         {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
+            "input": 2.2,
+            "answer": [4, 20]
+        },
+        {
+            "input": 3.3,
+            "answer": [24, 28]
+        },
+        {
+            "input": 0.9,
+            "answer": [0, 4]
+        },
+        {
+            "input": 2.7,
+            "answer": [12, 20]
+        },
+        {
+            "input": 3.4,
+            "answer": [24, 28]
+        },
+        {
+            "input": 0.1,
+            "answer": [0, 4]
         }
     ]
 }
